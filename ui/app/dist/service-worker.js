ad9ed8ca48c4b71a2f015eb565665f8d78540a33
Ankit Anand <ankit.anand@oracle.com>
2021-09-24 14:27:13 +0530

    chore: addinng dist

---
+++ ui/app/dist/service-worker.js
@@ -0,0 +1,135 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  const singleRequire = name => {
+    if (name !== 'require') {
+      name = name + '.js';
+    }
+    let promise = Promise.resolve();
+    if (!registry[name]) {
+      
+        promise = new Promise(async resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = name;
+            document.head.appendChild(script);
+            script.onload = resolve;
+          } else {
+            importScripts(name);
+            resolve();
+          }
+        });
+      
+    }
+    return promise.then(() => {
+      if (!registry[name]) {
+        throw new Error(`Module ${name} didn’t register its module`);
+      }
+      return registry[name];
+    });
+  };
+
+  const require = (names, resolve) => {
+    Promise.all(names.map(singleRequire))
+      .then(modules => resolve(modules.length === 1 ? modules[0] : modules));
+  };
+  
+  const registry = {
+    require: Promise.resolve(require)
+  };
+
+  self.define = (moduleName, depsNames, factory) => {
+    if (registry[moduleName]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    registry[moduleName] = Promise.resolve().then(() => {
+      let exports = {};
+      const module = {
+        uri: location.origin + moduleName.slice(1)
+      };
+      return Promise.all(
+        depsNames.map(depName => {
+          switch(depName) {
+            case "exports":
+              return exports;
+            case "module":
+              return module;
+            default:
+              return singleRequire(depName);
+          }
+        })
+      ).then(deps => {
+        const facValue = factory(...deps);
+        if(!exports.default) {
+          exports.default = facValue;
+        }
+        return exports;
+      });
+    });
+  };
+}
+define("./service-worker.js",['./workbox-8e713196'], (function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+
+  workbox.precacheAndRoute([{
+    "url": "android-chrome-192x192.png",
+    "revision": "03e8c7dbc01f949eefe3fe27fe1a0a95"
+  }, {
+    "url": "android-chrome-512x512.png",
+    "revision": "465e128b66c3b8cdbb440957df5d9d72"
+  }, {
+    "url": "app.js",
+    "revision": "3e65d8d80c285f5c5c49cd6342fc16ac"
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "7c3ad9066e3d99d348c4143464a7bfee"
+  }, {
+    "url": "favicon-16x16.png",
+    "revision": "c93bfb602eed2baa9bc0fdae53abed95"
+  }, {
+    "url": "favicon-32x32.png",
+    "revision": "793ac91287ab988ebcc1510d662f7a49"
+  }, {
+    "url": "favicon.ico",
+    "revision": "debb45cd39db5b0203457c33076c4b74"
+  }, {
+    "url": "index.html",
+    "revision": "2014b01d1480eb9079a74a6a20ee7480"
+  }, {
+    "url": "manifest.json",
+    "revision": "aff0e706e192b58b0df9db0066c45e78"
+  }], {});
+
+}));
